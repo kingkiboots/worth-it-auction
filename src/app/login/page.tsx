e11cbc0262387ec62c3b425ba73caf349a57fc4a
kingkiboots <tuffhr4@gmail.com>
2026-07-12 00:30:55 +0900

    feat: 로그인 화면 진입 시 로그인 되어있으면 보드 화면으로 리디렉션 처리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,17 @@
 import { KakaoLoginButton } from "@/features/auth/ui/KakaoLoginButton";
+import { ROUTES } from "@/shared/config/routes";
+import { createServerSideClient } from "@/shared/db/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  // 1. 서버에서 현재 로그인한 유저 정보 확인
+  const supabase = await createServerSideClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) redirect(ROUTES.AUCTION);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-2xl shadow-lg">
